e56fc90402a02ca2452c47c0ff72907240e221ae
mxriopm <2336000502@utna.edu.mx>
2026-03-19 19:13:02 -0600

    Completo npm start

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '', // pon tu contraseña si tienes
+    password: '',
     database: 'erp_db'
 });
 
